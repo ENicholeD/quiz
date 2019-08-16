3c25276d75b37f572580524f6474d1c0343c51b8
Erin Deuel <end.gaia@gmail.com>
2019-08-16 12:07:45 -0700

    add answer section

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,15 @@ $(document).ready(function(){
     var time = parseInt($("#time").val());
     var calculate = travel + fruit + game + sea + time;
     event.preventDefault();
-    console.log(calculate);
+
+    if (calculate === 4 || calculate === 5 || calculate === 6 || calculate === 7 ) {
+      console.log(calculate);
+    } else if (calculate === 8 || calculate === 9 || calculate === 10 || calculate === 11){
+      console.log("HELLO");
+    } else if (calculate === 12 || calculate === 13 || calculate === 14 || calculate === 15){
+      console.log(calculate);
+    } else {
+      
+    }
   });
 });
